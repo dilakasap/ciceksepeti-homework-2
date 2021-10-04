8d12e4ac21da1bfd55032dc7be4ef3064ec4af1e
dilakasap <dilakasap@gmail.com>
2021-10-04 17:51:20 +0300

    last changes

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# ciceksepeti-homework-
+# Çiçeksepeti React Bootcamp-Homework-2
 For live Url:
 https://ciceksepeti-homework-2.vercel.app/
+
+![Vercel](http://therealsujitk-vercel-badge.vercel.app/?app=ciceksepeti-homework-2)
+
+<h3>Used Technologies</h3>
+<hr>
+Node<br>
+Webpack<br>
+Babel<br>
+Eslint<br>
+OOP<br>
+Vercel for deployment<br>
+
+
+
--- src/js/models/duck.js
+++ src/js/models/duck.js
@@ -9,32 +9,19 @@ class Duck extends Animal {
 
   // create method which create elements
   create() {
-    const age = document.createElement("p");
-    age.innerHTML = this.age;
-    age.className = "age";
-    const numberOfLegs = document.createElement("p");
-    numberOfLegs.innerHTML = "Number of legs:" + this.numberOfLegs;
-    numberOfLegs.className = "numberOfLegs";
-    const name = document.createElement("p");
-    name.innerHTML = this.name;
-    name.className = "name";
-    const image = document.createElement("div");
-    image.className = "image";
-    const img = document.createElement("img");
-    img.src = this.image;
-    const info = document.createElement("div");
-    info.className = "info";
     const card = document.createElement("div");
     card.className = "card";
-
-    info.appendChild(name);
-    info.appendChild(numberOfLegs);
-    info.appendChild(age);
-    image.appendChild(img);
-    card.appendChild(image);
-    card.appendChild(info);
+    card.innerHTML = `
+  <div class="image">
+    <img src=${this.image}>
+  </div>
+  <div class="info">
+    <p class="name">${this.name}</p>
+    <p class="numberOfLegs">Number of Legs:${this.numberOfLegs}</p>
+    <p class="age">${this.age}</p>
+  </div>
+  `;
     return card;
   }
 }
-
 export default Duck;
